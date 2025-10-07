aaf7a6b6d4f98523e5920c4b0019f0b99744a160
Szymon Turek <szymon.codes@gmail.com>
2025-10-07 14:35:12 +0200

    Dodanie generycznego repozytorium oraz utworzenie pierwszego repozytorium product - dokonczyc

--- storage/webapp/database/models/products.py
+++ storage/webapp/database/models/products.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from sqlalchemy import String, DateTime, Integer, Numeric, Text, Boolean, func
+from sqlalchemy import String, DateTime, Integer, Numeric, Text, Boolean, func, CheckConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ...extensions import db
+
 from typing import TYPE_CHECKING
 
 # Ten blok jest widoczny tylko dla edytora kodu, a nie dla Pythona podczas uruchamiania.
@@ -22,6 +23,7 @@ class Product(db.Model):# type: ignore
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     is_expiration_date: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
     is_dose_product: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    days_of_dosage: Mapped[int] = mapped_column(Integer, nullable=True)
     is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
     description: Mapped[str] = mapped_column(Text, nullable=True, default=None)
 
@@ -34,6 +36,12 @@ class Product(db.Model):# type: ignore
         server_default=func.now(),
         onupdate=func.now()
     )
+
+    __table_args__ = (CheckConstraint('(is_dose_product = true and days_of_dosage > 0) or'
+                                      '(is_dose_product = false and days_of_dosage is null)',
+                                      name='chk_days_of_dosage_of_product'),
+                      )
+
 
     products_suppliers_info: Mapped[list['ProductSupplierInfo']] = relationship(
         back_populates="products"
---
+++ storage/webapp/database/repositories/products.py
@@ -0,0 +1,74 @@
+from sqlalchemy import select, update
+from .generic import GenericRepository
+from ..models.products import Product
+from ...extensions import db
+
+
+
+class ProductRepository(GenericRepository[Product]):
+    def __init__(self):
+        super().__init__(Product)
+
+
+# ------------------------ Aktualizacje statusow ------------------------
+
+    def set_exp_date_status(self, product: Product, status: bool) -> None:
+        product.is_expiration_date = status
+
+    """ Wykonuje PRAWDZIWĄ masową aktualizację statusu 'is_expiration_date' dla produktów.
+    Generuje jedno zapytanie UPDATE w bazie danych.
+    Zwraca liczbę zaktualizowanych wierszy.
+    """
+
+    def bulk_exp_date_status(self, skus: list[str], status: bool) -> None:
+        stmt = (
+            update(Product)
+            .where(Product.id.in_(list(skus)))
+            .values(is_expiration_date=status))
+
+        result = db.session.execute(stmt)
+
+        return result.rowcount
+
+
+
+    def set_active_status(self, product: Product, status: bool) -> None:
+        product.is_active = status
+
+
+    def bulk_active_status(self, skus: list[str], status: bool) -> None:
+        stmt = (
+            update(Product)
+            .where(Product.id.in_(list(skus)))
+            .values(is_active=status))
+
+        result = db.session.execute(stmt)
+
+        return result.rowcount
+
+
+    def set_dose_product_status(self, product: Product, status: bool, dosage: int | None) -> None:
+        product.is_dose_product = status
+        if status:
+            if dosage is None or dosage <= 0:
+                raise ValueError("Dosage must be greater than 0 if status is True")
+            product.days_of_dosage = dosage
+        else:
+            # zwracany jest None bo w tabeli jest Null i trzeba to tak ustawic by nie bylo bledow
+            product.days_of_dosage = None #type: ignore
+
+
+    def update_description(self, product: Product, description: str) -> None:
+        product.description = description
+
+
+# ------------------------ Filtrowanie ------------------------
+
+    def get_by_sku(self, sku: int) -> Product | None:
+        stmt = select(Product).where(Product.sku == sku)
+        return db.session.scalar(stmt).first()
+
+    def get_all_by_active_status(self, status: bool) -> list[Product] | None:
+        stmt = select(Product).where(Product.is_active.is_(status))
+        #alternatywa stmt = select(Product).where(Product.is_active == status)
+        return list(db.session.scalars(stmt))
